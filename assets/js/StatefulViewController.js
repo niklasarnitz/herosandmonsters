dc58cb2c79197a3e5ad26c50dad6c6a0ade427ae
Niklas <niklas.arnitz@icloud.com>
2020-03-29 16:28:36 +0200

    Fixed a final bug

--- assets/js/StatefulViewController.js
+++ assets/js/StatefulViewController.js
@@ -9,12 +9,28 @@ class StatefulViewController {
     }
 
     updateView() {
+	if(this.viewModel.getHerosLifepoints() <= 1) alert("GAME OVER!")
+	if(this.viewModel.getMonstersLifepoints() <= 1) alert("Du hast die Monster besiegt und das Königreich besiegt!")
         this.view.roundLabel.html(this.viewModel.round + ". Runde")
         this.view.heroLifePointsLabel.html(this.viewModel.getHerosLifepoints())
         this.view.monstersLifepointsLabel.html(this.viewModel.getMonstersLifepoints())
         this.view.herosConditionScrollBox.html(this.viewModel.getHerosCondition())
         this.view.monstersConditionScrollBox.html(this.viewModel.getMonstersCondition())
-        this.view.enemyLabel.html("Dein Gegner: " + this.viewModel.monsters[this.viewModel.monsterIndex].getCondition())
+        if(this.viewModel.monsters[this.viewModel.monsterIndex] == undefined) {
+		this.view.enemyLabel.html("DU HAST GESIEGT!")
+		this.view.monstersConditionScrollBox.html("")
+		this.view.monstersLifepointsLabel.html("Gesamte Monsterleben: 0")
+		alert("Du hast die bösen Monster besiegt und das Königkreich gerettet!!")
+	} else {
+		this.view.enemyLabel.html("Dein Gegner: " + this.viewModel.monsters[this.viewModel.monsterIndex].getCondition())
+    	}
+
+	if(this.viewModel.heros[this.viewModel.heroIndex] == undefined) {
+                this.view.enemyLabel.html("GAME OVER! Du hast leider verloren")
+                this.view.herosConditionScrollBox.html("")
+                this.view.heroLifePointsLabel.html("Gesamte Heroleben: 0")
+                alert("GAME OVER! Du hast leider verloren.")
+        }
     }
 
     fight() {
@@ -23,6 +39,7 @@ class StatefulViewController {
         this.viewModel.round++;
         this.viewModel.chooseNewMonster()
         this.viewModel.fight()
-        this.updateView();
+        this.viewModel.chooseNewMonster()
+	this.updateView();
     }
 }
